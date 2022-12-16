ca047aaa507be7e78dd9a8e7da1e8745ca5bea80
leonardevans <lemutugi@gmail.com>
2022-12-16 08:02:21 -0500

    update

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -3,14 +3,17 @@ import { shallow } from 'enzyme';
 import CounterButton from './CounterButton';
 
 it('renders without crashing', () => {
-  expect(shallow(<CounterButton />)).toMatchSnapshot();
+  expect(shallow(<CounterButton color='red'/>)).toMatchSnapshot();
 });
 
 it('correnctly increments the counter', () => {
   const wrapper = shallow(<CounterButton/>)
+
   expect(wrapper).toMatchSnapshot();
+
   wrapper.find('[id="counter"]').simulate('click');
   expect((wrapper.state())).toEqual({count: 1})
+
   wrapper.find('[id="counter"]').simulate('click');
   wrapper.find('[id="counter"]').simulate('click');
   expect((wrapper.state())).toEqual({count: 3})
